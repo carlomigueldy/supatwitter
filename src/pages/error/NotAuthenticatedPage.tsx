8422ae7ebf1cd57635f05f460ea89aec3e1521ae
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-09-10 11:12:40 +0800

    Add error pages

---
+++ src/pages/error/NotAuthenticatedPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotAuthenticatedPage() {
+  return (
+    <>
+      <h1>Not Authenticated</h1>
+    </>
+  );
+}
---
+++ src/pages/error/NotAuthorizedPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotAuthorizedPage() {
+  return (
+    <>
+      <h1>Not Authorized</h1>
+    </>
+  );
+}
