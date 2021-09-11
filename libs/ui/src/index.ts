73145be2acb2a4f7b043e6e5fccad055cf72b331
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-09-11 22:17:47 +0800

    Add ui lib

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/primary-button/primary-button';
+export * from './lib/ui';
---
+++ libs/ui/src/lib/primary-button/primary-button.tsx
@@ -0,0 +1,14 @@
+import './primary-button.module.scss';
+
+/* eslint-disable-next-line */
+export interface PrimaryButtonProps {}
+
+export function PrimaryButton(props: PrimaryButtonProps) {
+  return (
+    <div>
+      <h1>Welcome to PrimaryButton!</h1>
+    </div>
+  );
+}
+
+export default PrimaryButton;
