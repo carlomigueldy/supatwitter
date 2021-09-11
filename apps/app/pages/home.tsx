d629c0a1a335289ee82433301118b9ddcd71ae77
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-09-11 22:18:05 +0800

    Add chakra-ui dependency

--- apps/app/pages/home.tsx
+++ apps/app/pages/home.tsx
@@ -1,7 +1,11 @@
+import { PrimaryButton } from '@supatwitter/ui';
+
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
+
+      <PrimaryButton></PrimaryButton>
     </>
   );
 }
